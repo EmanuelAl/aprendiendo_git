8a62e59945c917ef7569585ad2cac35a0b5bab40
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-01-13 12:52:07 -0300

    "se resetea identidad de author"

---
+++ prueba.js
@@ -0,0 +1,14 @@
+
+var var1=0;
+var var2=3;
+var nombre;
+
+
+nombre = prompt("ingrese su nombre");
+
+document.write("hola" + " " + nombre);
+
+while (var1<var2) {
+	console.log("hola"+ var1);
+	var1= var1 + 1;
+}
